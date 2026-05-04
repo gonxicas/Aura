f31623267666b3503557d8858cfa546d2d4b7844
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-05-04 20:12:12 +0900

    feat: create player and enemy classes

---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,24 @@
+﻿#include "Character/AuraEnemy.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAuraEnemy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAuraEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
