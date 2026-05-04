866cd237bbfd5013d2370ef4eae5a09c3b48f134
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-05-04 22:03:15 +0900

    feat(AuraPlayerController): set up move behaviour

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -1,5 +1,6 @@
 ﻿#include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 
 class UEnhancedInputLocalPlayerSubsystem;
@@ -27,4 +28,30 @@ void AAuraPlayerController::BeginPlay()
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	auto EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	
+	
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const auto InputAxisVector = InputActionValue.Get<FVector2D>();
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+	
+	const auto ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const auto RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	if(auto ControllerPawn = GetPawn<APawn>())
+	{
+		ControllerPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControllerPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+	
 }
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,6 +4,7 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputAction;
 class UInputMappingContext;
 
 UCLASS()
@@ -14,13 +15,18 @@ class AURA_API AAuraPlayerController : public APlayerController
 protected:
 
 	virtual void BeginPlay() override;
-
+	virtual void SetupInputComponent() override;
+	
 public:
 	AAuraPlayerController();
-	
 
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	
+	void Move(const struct FInputActionValue& InputActionValue);
 	
 };
