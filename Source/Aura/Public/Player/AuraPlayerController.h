eba944a9f1f42538d4175056748fafe360fc9d21
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-05-04 21:44:54 +0900

    feat: Setup AuraPlayerController

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,8 +4,23 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+	
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+	AAuraPlayerController();
+	
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+	
 };
