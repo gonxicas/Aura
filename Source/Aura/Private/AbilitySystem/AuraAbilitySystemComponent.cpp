3321b7086373deed37c50a954349d834e4bad77a
Gonzalo Casin <gcasinbricio@hotmail.com>
2026-05-09 19:39:26 +0900

    feat: create custom Ability System Component and Attribute Set. Add needed modules

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,25 @@
+﻿#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
+
+UAuraAbilitySystemComponent::UAuraAbilitySystemComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+
+void UAuraAbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+
+void UAuraAbilitySystemComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
+
